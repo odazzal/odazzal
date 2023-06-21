c05c85652b1b2131b89cb7c2e18c7b3efce0224c
DELL <DELL@DESKTOP-1F62Q91>
2023-06-21 22:27:47 +0530

    Frist commit

---
+++ ODazzalDevops/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ODazzalDevops.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
